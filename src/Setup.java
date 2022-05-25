79e7ab3c30e25bf21ded5f2aaf34648955703f94
BT_1E10_06 <sample@bths.edu>
2022-05-25 12:25:18 -0400

    Check code for functioning errors.

--- src/Setup.java
+++ src/Setup.java
@@ -24,16 +24,17 @@ public class Setup {
             System.out.println(" -----Task Planner----- ");
             Scanner s = new Scanner(System.in);
             //Ask about categories and keep adding, prints the complete list each time a complete addition is added. There will also be an option later on the fix if necessary (delete / replace).
-            System.out.println("Input individual categories (folder). Enter \"x\" when completed");
+            System.out.println("Input individual categories (folder). Enter \"x\" when completed. Enter \" \" to continue and \"x\" upon completion.");
+            System.out.println("Enter category name: ");
             String in = s.nextLine();
             while (!in.toLowerCase().equals("x")) {
+                categories.add(in);
                 System.out.println("Enter category name: ");
-                categories.add(s.nextLine());
-                System.out.println("Enter \" \" to continue or \"x\" to stop.");
+                in = s.nextLine();
             }
             System.out.println("Please add your tasks. Enter \"s\" to stop");
             while (! s.nextLine().toLowerCase().equals("s")) {
-                create.action();
+                create.action(categories);
             }
         }
     }
